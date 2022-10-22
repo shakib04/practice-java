8465bd7fa1e9a3fcf6d04054a3dabee9b236baa5
shakibul <shakibul.alam@bracits.com>
2022-10-22 13:45:46 +0600

    create directory and file

--- src/main/java/practice/io/FileCRUD.java
+++ src/main/java/practice/io/FileCRUD.java
@@ -1,6 +1,7 @@
 package practice.io;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileCRUD {
 
@@ -9,8 +10,19 @@ public class FileCRUD {
         return System.getProperty("user.dir");
     }
 
-    public static File createAFile(String directory, String fileName) {
-        return new File(directory + File.pathSeparator + fileName);
+    public static boolean createAFile(String directory, String fileName) throws IOException{
+        File file = new File(directory + File.separator + fileName);
+        boolean result = file.createNewFile();
+        System.out.println("File Path: " + file.getPath());
+        return result;
+    }
+
+    public static String createDirectory(String directory) {
+        File dir = new File(directory);
+        dir.mkdir();
+        String path = dir.getPath();
+        System.out.println("New Directory path: " + path);
+        return path;
     }
 }
 
@@ -19,8 +31,14 @@ public class FileCRUD {
  */
 class DriverClass {
 
-    public static void main(String[] args) {
-        String directory = FileCRUD.getWorkingDirectory();
-        FileCRUD.createAFile(directory, "hello.txt");
+    public static void main(String[] args) throws IOException {
+        //String directory = FileCRUD.getWorkingDirectory();
+        //System.out.println(directory);
+
+        // lets create a new Directory
+        String directory = FileCRUD.createDirectory("hello1");
+        
+        // lets create a new file
+        System.out.println(FileCRUD.createAFile(directory, "hello.txt"));
     }
 }
