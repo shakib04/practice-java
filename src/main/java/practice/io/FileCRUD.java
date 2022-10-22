5d1bcb50af1df00d4cd7ba879dd1a5e2311f597d
shakibul <shakibul.alam@bracits.com>
2022-10-22 14:00:43 +0600

    file rename complete

--- src/main/java/practice/io/FileCRUD.java
+++ src/main/java/practice/io/FileCRUD.java
@@ -10,11 +10,15 @@ public class FileCRUD {
         return System.getProperty("user.dir");
     }
 
-    public static boolean createAFile(String directory, String fileName) throws IOException{
+    public static File prepareAFile(String directory, String fileName) {
+        return new File(directory + File.separator + fileName);
+    }
+
+    public static String createAFile(String directory, String fileName) throws IOException {
         File file = new File(directory + File.separator + fileName);
-        boolean result = file.createNewFile();
+        file.createNewFile();
         System.out.println("File Path: " + file.getPath());
-        return result;
+        return file.getPath();
     }
 
     public static String createDirectory(String directory) {
@@ -24,6 +28,15 @@ public class FileCRUD {
         System.out.println("New Directory path: " + path);
         return path;
     }
+
+    public static void renameFile(File oldFile, File newFile) {
+        boolean result = oldFile.renameTo(newFile);
+        if(result){
+            System.out.println("new file path: " + newFile);
+        }else{
+            System.out.println("failed to rename");
+        }
+    }
 }
 
 /**
@@ -32,13 +45,18 @@ public class FileCRUD {
 class DriverClass {
 
     public static void main(String[] args) throws IOException {
-        //String directory = FileCRUD.getWorkingDirectory();
-        //System.out.println(directory);
+        // String directory = FileCRUD.getWorkingDirectory();
+        // System.out.println(directory);
 
         // lets create a new Directory
         String directory = FileCRUD.createDirectory("hello1");
-        
+
         // lets create a new file
-        System.out.println(FileCRUD.createAFile(directory, "hello.txt"));
+        String fileName = "hello.txt";
+        String path = FileCRUD.createAFile(directory, fileName);
+
+        // lets rename a file
+        File newFile = FileCRUD.prepareAFile(directory, "new.txt");
+        FileCRUD.renameFile(new File(path), newFile);
     }
 }
