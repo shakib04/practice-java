73b791bad0401690451bbb071eefbdc9f0083f8b
shakibul <mdshakibulalam@gmail.com>
2023-11-14 08:34:16 +0600

    some dummy files added to keep the directory non-empty and alive in git

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import practice.HelloWorld;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testMain(){
+        HelloWorld.main(new String[]{"abc"});
+    }
+}
