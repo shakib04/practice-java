5140bed40ef5f7a193fbd8a52daa25e9b1dd27eb
shakibul <mdshakibulalam@gmail.com>
2023-12-29 01:56:21 +0600

    implement InsertionSort in java

--- src/main/java/practice/sorting/InsertionSort.java
+++ src/main/java/practice/sorting/InsertionSort.java
@@ -5,21 +5,26 @@ import java.util.Arrays;
 public class InsertionSort {
 
     static void sort(int[] items) {
+        int[] sorted = new int[items.length];
+        sorted[0] = items[0];
+
         for (int i = 1; i < items.length; i++) {
-            for (int j = i - 1; j > 0; j--) {
-                if (items[i] < items[j]) {
-                    int temp = items[j];
-                    items[j] = items[i];
-                    items[i] = temp;
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (items[i] < sorted[j]) {
+                    sorted[j + 1] = sorted[j];
+                } else {
+                    break;
                 }
             }
+            sorted[j + 1] = items[i];
         }
-        System.out.println(Arrays.toString(items));
+        System.out.println(Arrays.toString(sorted));
     }
 
     public static void main(String[] args) {
         sort(new int[]{6, 3, 0, 5});
-        sort(new int[]{64, 25, 3, 12, 22, 11, 6});
+        sort(new int[]{64, 25, 3, 12, 22, 11, 6, 6,91, 98});
         //sortDesc(new int[]{64, 25, 3, 12, 22, 11, 6});
     }
 
